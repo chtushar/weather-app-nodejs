f691867582efbc00d7360272843f29b5bb0cc0c4
chtushar <choudharit23@gmail.com>
2020-03-18 21:53:51 +0530

    new features added

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,9 @@
 const form = document.querySelector("form");
 const search = form.querySelector("input");
 const report = document.querySelector(".report");
-const reportLocation = document.querySelector(".report-location");
+const loc = document.querySelector(".location");
+const outputPrimary = document.querySelector(".output-primary");
+const outputSecondary = document.querySelector(".output-secondary");
 
 form.addEventListener("submit", e => {
   e.preventDefault();
@@ -11,15 +13,22 @@ form.addEventListener("submit", e => {
   fetch(path).then(response => {
     response.json().then(data => {
       if (data.error) {
-        reportLocation.innerHTML = "";
-        report.innerHTML = "";
-        report.innerHTML = data.error;
+        outputPrimary.innerHTML="";
+        outputSecondary.innerHTML = "";
+        loc.innerHTML = "";
+        loc.innerHTML = data.error;
       } else {
-        report.innerHTML = "";
-        //report.append(data.location);
-        reportLocation.innerHTML = "";
-        reportLocation.innerHTML = data.location;
-        report.innerHTML = data.forecast;
+        //let loc = data.location.split(',')[0];
+        loc.innerHTML = "";
+        loc.innerHTML = `<img src="/img/location-point.svg"/>
+                              <p class="location-text">${data.location.split(',')[0]}</p>`;
+        outputPrimary.innerHTML = "";
+        outputSecondary.innerHTML = "";
+        outputPrimary.innerHTML = `<div class="temperature">${data.forecast.temperature.toPrecision(3)}<sup style="font-size: 3rem;">&#8451;</sup></div>
+                                   <div class="forecast"><img src="/img/icons/${data.forecast.icon}.svg"/></div>`;
+        outputSecondary.innerHTML = `<div class="rain-prob">Rain Probability ${100*data.forecast.precipProbability.toPrecision(4)}%</div>
+                                     <div class="wind-speed">Wind speed ${data.forecast.windSpeed} kmph</div>`;
+        
       }
     });
   });
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,8 +14,8 @@ const forecast = (d, callback) => {
         else if(response.body.error){
             callback('Unable to find Location', undefined);
         }else {
-            const data = response.body.daily.data[0].summary +` It is currently ${response.body.currently.temperature} degrees out. There's ${100*response.body.currently.precipProbability.toPrecision(2)}% chance of rain.`;
-            callback(undefined, data);
+            //const data = response.body.daily.data[0].summary +` It is currently ${response.body.currently.temperature} degrees out. There's ${100*response.body.currently.precipProbability.toPrecision(2)}% chance of rain.`;
+            callback(undefined, response.body.currently);
         }
     })
 } 
