650b573bc10b66819af08faaa13f96c65aae7066
chtushar <choudharit23@gmail.com>
2020-03-18 14:03:53 +0530

    setup for heroku app

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,26 @@
-const form = document.querySelector('form');
-const search = form.querySelector('input');
-const report = document.querySelector('.report');
-const reportLocation = document.querySelector('.report-location');
+const form = document.querySelector("form");
+const search = form.querySelector("input");
+const report = document.querySelector(".report");
+const reportLocation = document.querySelector(".report-location");
 
-form.addEventListener('submit',(e)=>{
-    e.preventDefault()
-    const location = search.value;
-    const path = `${document.URL}weather?address=${location}`;
-    getDataFromServer(path);
-})
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const location = search.value;
+  const path = `/weather?address=${location}`;
 
-
-function getDataFromServer(path) {
-    
-fetch(path).then((response) => {
-        response.json().then((data) => {
-            if(data.error){
-                
-                reportLocation.innerHTML = '';
-                report.innerHTML = '';
-                report.innerHTML = data.error;
-            }
-            else{
-                report.innerHTML = '';
-                //report.append(data.location);
-                reportLocation.innerHTML = '';
-                reportLocation.innerHTML = data.location;
-                report.innerHTML = data.forecast;
-            }
-        })
-    })
-}
+  fetch(path).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        reportLocation.innerHTML = "";
+        report.innerHTML = "";
+        report.innerHTML = data.error;
+      } else {
+        report.innerHTML = "";
+        //report.append(data.location);
+        reportLocation.innerHTML = "";
+        reportLocation.innerHTML = data.location;
+        report.innerHTML = data.forecast;
+      }
+    });
+  });
+});
